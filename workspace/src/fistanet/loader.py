361f07902140208225cf01e49fe1d8086347add3
Gergő Galiger <galigergergo@yahoo.com>
2024-04-07 13:34:57 +0200

    Adapted rest of the code to handle the new data format

--- workspace/src/fistanet/loader.py
+++ workspace/src/fistanet/loader.py
@@ -10,9 +10,14 @@ DataLoader of ECG data.
 from torch.utils.data import Dataset, DataLoader
 from torch.utils.data.sampler import SubsetRandomSampler
 from os.path import join as pjoin
+import pickle as pkl
 import numpy as np
+import wfdb
+from wfdb import processing
 
-class ECGDataset(Dataset):
+
+# Reads large np array input data - inefficient
+class ECGDatasetOld(Dataset):
     def _calc_split_params(self, mode, tot_smpl_no, tvt_split):
         smpl_start_i = 0
         split_norm = 0
@@ -49,10 +54,67 @@ class ECGDataset(Dataset):
         return self.data_[0, :, idx], self.data_[1, :, idx]
 
 
-def DataSplit(data_dir, file_name, tvt_split, batch_size=128, transform=None):
-    ds_trn = ECGDataset('train', data_dir, file_name, tvt_split, transform)
-    ds_val = ECGDataset('valid', data_dir, file_name, tvt_split, transform)
-    ds_tst = ECGDataset('test', data_dir, file_name, tvt_split, transform)
+# Reads efficient input data of cut indexes
+class ECGDataset(Dataset):
+    def _calc_split_params(self, mode, tot_smpl_no, tvt_split):
+        smpl_start_i = 0
+        split_norm = 0
+        res_dct = dict()
+        for m in ['train', 'valid', 'test']:
+            split_norm += tvt_split[m] / sum(tvt_split.values())
+            last_smpl_i = int(np.floor(tot_smpl_no * split_norm))
+            if m == mode:
+                return smpl_start_i, last_smpl_i - smpl_start_i
+            smpl_start_i = last_smpl_i
+    
+    def __init__(self, mode, data_dir, file_gen, file_sigs, file_bw, tvt_split, transform=None):
+        assert mode in ['train', 'valid', 'test']
+        self.mode = mode
+        
+        self.inp_dir = data_dir
+        self.file_gen = file_gen
+        self.file_sigs = file_sigs
+        self.file_bw = file_bw
+        self.transform = transform
+
+        # load data files
+        with open(pjoin(self.inp_dir, self.file_gen), 'rb') as file:
+            data_temp_ = pkl.load(file)
+        self.data_sigs_ = np.load(pjoin(self.inp_dir, self.file_sigs))
+        bw_temp_ = wfdb.rdrecord(pjoin(self.inp_dir, self.file_bw))
+
+        # save data generation parameters
+        self.tot_smpl_no = data_temp_['params']['GEN_DATA_SIZE']
+        self.inp_smpl_freq = data_temp_['params']['INP_SMPL_FREQ']
+        self.gen_sig_secs = data_temp_['params']['GEN_SIG_SECS']
+        self.valid_inp_sigs = data_temp_['params']['VALID_INP_SIGS']
+        self.gen_sig_len = self.inp_smpl_freq * self.gen_sig_secs
+
+        # resample second channel of bw noise data to the input signal frequency
+        self.data_bw_, _ = processing.resample_sig(bw_temp_.p_signal[:, 1], bw_temp_.fs, self.inp_smpl_freq)
+        
+        # perform tvt split
+        smpl_start_i, self.smpl_no = self._calc_split_params(mode, self.tot_smpl_no, tvt_split)
+        self.data_cuts_ = np.int_(data_temp_['data'][:, smpl_start_i:smpl_start_i+self.smpl_no])
+        print(self.data_cuts_)
+        print(self.data_cuts_[1, 0], self.data_cuts_[1, 0]+self.gen_sig_len, self.data_cuts_[0, 0])
+        
+        # transform the input tensor into required formats
+        if self.transform:
+            input_m = self.transform(input_m)
+     
+    def __len__(self):
+        return self.smpl_no
+
+    def __getitem__(self, idx):
+        sig_item = self.data_sigs_[self.data_cuts_[1, idx]:self.data_cuts_[1, idx]+self.gen_sig_len, self.data_cuts_[0, idx]]
+        return sig_item, sig_item + self.data_bw_[self.data_cuts_[2, idx]:self.data_cuts_[2, idx]+self.gen_sig_len]
+
+
+def DataSplit(data_dir, file_gen, file_sigs, file_bw, tvt_split, batch_size=128, transform=None):
+    ds_trn = ECGDataset('train', data_dir, file_gen, file_sigs, file_bw, tvt_split, transform)
+    ds_val = ECGDataset('valid', data_dir, file_gen, file_sigs, file_bw, tvt_split, transform)
+    ds_tst = ECGDataset('test', data_dir, file_gen, file_sigs, file_bw, tvt_split, transform)
 
     trn_smplr = SubsetRandomSampler(list(range(len(ds_trn))))
 
